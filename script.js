16ffbdd266548722006eef6ee96de5dbc919dee4
Şimal Bülbül <161174055+simalbulbul@users.noreply.github.com>
2025-01-16 06:41:24 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-const menuIcon = document.getElementById("menu-icon");
-const navLinks = document.querySelector(".nav-links");
-
-menuIcon.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
